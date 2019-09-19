dedbf5d8f0f07b996f736dc040ab0428087fe6b6
Grace <gsyed1120@gmail.com>
2019-09-19 22:01:13 +0100

    day 10/100

--- Vue/section5/app.js
+++ Vue/section5/app.js
@@ -1,13 +1,19 @@
+var data = {
+  title: 'The VueJS Instance',
+  showParagraph: false
+};
+
+Vue.component('hello', {
+  template: '<h1>Hello</h1>'
+});
+
 var vm1 = new Vue({
-  el: "#app1",
-  data: {
-    title: 'The VueJS Instance',
-    showParagraph: false
-  },
+  data: data,
   methods: {
     show: function() {
       this.showParagraph = true;
       this.updateTitle('The VueJS Instance (Updated)');
+
     },
     updateTitle: function(title) {
       this.title = title;
@@ -25,8 +31,13 @@ var vm1 = new Vue({
   }
 });
 
+vm1.$mount('#app1');
+
+vm1.$refs.heading.innerText = "Something Else";
+
 setTimeout(function(){
   vm1.title = "changed by timer";
+  vm1.show();
 }, 1000);
 
 var vm2 = new Vue({
@@ -39,4 +50,11 @@ var vm2 = new Vue({
       vm1.title = "changed";
     }
   }
-})
+});
+
+var vm3 = new Vue({
+  el: 'hello',
+  template: '<h1>Hello</h1>'
+});
+
+// vm3.$mount('#app3');
