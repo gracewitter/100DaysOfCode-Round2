3bbdfd59935724859df0381b18e6922c1e6cc600
Grace <gsyed1120@gmail.com>
2019-10-02 22:42:21 +0100

    day 12/100

--- Vue/section7-components/app.js
+++ Vue/section7-components/app.js
@@ -1,14 +1,26 @@
-Vue.component("my-cmp", {
+var cmp = {
   data: function() {
     return {
       status: "critical"
     };
   },
-  template: "<p>Server Status: {{status }}</p>"
-});
+  template: "<p>Server Status: {{status }} (<button @click='changeStatus'>Change</button>)</p>",
+  methods: {
+    changeStatus: function() {
+      this.status = "Normal";
+    }
+  }
+};
 
 
 new Vue({
   el: "#app",
+  components: {
+    'my-cmp': cmp
+  }
+});
+
+new Vue({
+  el: "#app2",
 
 });
--- Vue/vue-cli/src/App.vue
+++ Vue/vue-cli/src/App.vue
@@ -1,12 +1,30 @@
 <template>
-<h1>Hello Grace</h1>
+    <div class="container">
+      <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-servers-details></app-servers-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-export default {
+  import Header from './Header.vue';
+  import Servers from './Servers.vue';
+  import ServerDetails from './server-details.vue';
+  import Footer from './Footer.vue';
 
-  
-}
+  export default {
+    components: {
+        'app-header': Header,
+        'app-servers': Servers,
+        'app-servers-details': ServerDetails,
+        'app-footer': Footer,
+    }
+  }
 </script>
 
 <style>
