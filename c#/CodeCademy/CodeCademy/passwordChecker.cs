43c1ed63baa7bf429458bafd07d85630ed22c532
Grace Witter <grace@happyporch.com>
2020-05-27 15:28:39 +0100

    password checker

---
+++ c#/CodeCademy/CodeCademy/passwordChecker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCademy
+{
+    class passwordChecker
+    {
+        static void Main(string[] args)
+        {
+            int minLength = 8;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "0123456789";
+            string specialChars = "$%&*^#!?-";
+
+            Console.WriteLine("Enter a password");
+            string password = Console.ReadLine();
+
+            int score = 0;
+
+            if (password.Length >= minLength)
+            {
+                score++;
+            }
+            Console.WriteLine(score);
+
+            if (Tools.Contains(password, uppercase))
+            {
+                score++;
+            }
+            Console.WriteLine(score);
+
+            if (Tools.Contains(password, lowercase))
+            {
+                score++;
+            }
+            Console.WriteLine(score);
+
+            if (Tools.Contains(password, digits))
+            {
+                score++;
+            }
+            Console.WriteLine(score);
+
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            }
+            Console.WriteLine(score);
+
+
+            Console.ReadLine();
+        } 
+    }
+}
