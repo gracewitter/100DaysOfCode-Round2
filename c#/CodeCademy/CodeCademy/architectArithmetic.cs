bf0dcb849d70bd5cbf03d55fe3c54a67f0de2ffd
Grace Witter <grace@happyporch.com>
2020-05-31 09:09:19 +0100

    architect arithmetic

---
+++ c#/CodeCademy/CodeCademy/architectArithmetic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCademy
+{
+    class architectArithmetic
+    {
+        static void Main(string[] args)
+        {
+            double floorPlan = TriArea(750, 500) + (0.5 * CircleArea(375)) + RectangleArea(2500, 1500);
+            double cost = Math.Round((floorPlan * 180), 2);
+
+            Console.WriteLine($"The total cost for the floor plan is {cost}");
+
+            Console.ReadLine();
+        }
+        static double RectangleArea(double width, double length)
+        {
+            return width * length;
+        }
+        static double CircleArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        static double TriArea(double bottom, double height)
+        {
+            return 0.5 * bottom * height;
+        }
+    }
+}
