7ba47a55828c7d599bf45020482c89350ab3877f
Grace Witter <grace@happyporch.com>
2020-06-02 08:54:59 +0100

    caesar cipher

---
+++ c#/CodeCademy/CodeCademy/caesarCipher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCademy
+{
+    class caesarCipher
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.WriteLine("Enter a Secret Message");
+            string message = Console.ReadLine();
+
+            
+
+            char[] secretMessage = message.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char letter = secretMessage[i];
+                int letterIndex = Array.IndexOf(alphabet, letter);
+                int newIndex = (letterIndex + 3) % alphabet.Length;
+                char newLetter = alphabet[newIndex];
+                encryptedMessage[i] = newLetter;
+            }
+
+            Console.WriteLine(String.Join("", encryptedMessage));
+
+            Console.ReadLine();
+        }
+    }
+}
