de34a480fa31a0b372b8293ed9370797eb0d3221
gracewitter <32695869+gracewitter@users.noreply.github.com>
2020-06-15 11:12:42 +0100

    Create addition.php

---
+++ codecademy-projects/php/addition.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<?php
+$result = $_GET['num1'] + $_GET['num2'];
+echo "<p>".$_GET['num1']." + ".$_GET['num2']." = ".$result."</p>";
+?>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
