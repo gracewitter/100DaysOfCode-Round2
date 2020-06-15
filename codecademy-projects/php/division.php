d537d4f4e5c4c000dc6f52e79fb4cd27f4e254cf
gracewitter <32695869+gracewitter@users.noreply.github.com>
2020-06-15 11:12:57 +0100

    Create division.php

---
+++ codecademy-projects/php/division.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+$result = $_GET['numerator'] / $_GET['denominator'];
+echo "<p>".$_GET['numerator']." / ".$_GET['denominator']." = ".$result."</p>";
+?>
+
+
+
+<a href="index.php">Reset</a>
+</body>
+</html>
