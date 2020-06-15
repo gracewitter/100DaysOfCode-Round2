986db4a54c3c49675fc8108d861557136db03f73
gracewitter <32695869+gracewitter@users.noreply.github.com>
2020-06-15 11:12:22 +0100

    Create index.php

---
+++ codecademy-projects/php/index.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+<!--Your code goes here-->
+<h1>Addition</h1>
+<form method="GET" action="addition.php">
+    <label for="num1">First Number:</label>
+  <input type="number" name="num1">
+  <label for="num2">Second Number:</label>
+  <input type="number" name="num2">
+  <button type="sumbit" >Add</button>
+</form>
+
+<h1>Division</h1>
+<form method="GET" action="division.php">
+    <label for="numerator">Numerator:</label>
+    <input type="number" name="numerator">
+    <label for="denominator">Denominator</label>
+    <input type="number" name="denominator">
+    <button type="sumbit" >Divide</button>
+  </form>
+<a href="index.php">Reset</a>
+</body>
+</html>
